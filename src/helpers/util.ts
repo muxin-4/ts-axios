9bbbe4f56cf33494fb5564417d55da541eca0e29
王冠宇 <guanyu.wang@mobilemd.cn>
2022-10-04 06:07:05 +0800

    feat: 处理请求url参数 demo

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,9 +1,18 @@
 const toString = Object.prototype.toString
 
+/**
+ * 普通对象
+ * @param val
+ * @returns
+ */
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
+}
+
 export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
-export function isObject(val: any): val is Object {
-  return val !== null && typeof val === 'object'
-}
+// export function isObject(val: any): val is Object {
+//   return val !== null && typeof val === 'object'
+// }
