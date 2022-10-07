fcd353d3ec849972a73ae2685f0a41a5994fd72e
王冠宇 <guanyu.wang@mobilemd.cn>
2022-10-07 20:39:38 +0800

    feat: 扩展接口 接口类型定义

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export type method =
+export type Method =
   | 'get'
   | 'GET'
   | 'delete'
@@ -15,7 +15,7 @@ export type method =
   | 'PATCH'
 
 export interface AxiosRequestConfig {
-  url: string
+  url?: string
   method?: string
   data?: any
   params?: any
@@ -41,4 +41,28 @@ export interface AxiosError extends Error {
   code?: string | null
   request?: any
   response?: AxiosResponse
+}
+
+export interface Axios {
+  request(config: AxiosRequestConfig): AxiosPromise
+
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+}
+
+export interface AxiosInstance extends Axios {
+  (config: AxiosRequestConfig): AxiosPromise
+
+  (url: string, config?: AxiosRequestConfig): AxiosPromise
 }
