63d238e41f859290971c2beccc53c747d3a1d672
王冠宇 <wangguanyu@wangguanyudeMacBook-Pro.local>
2022-10-13 21:16:21 +0800

    feat: promise链式调用

--- src/types/index.ts
+++ src/types/index.ts
@@ -66,3 +66,15 @@ export interface AxiosInstance extends Axios {
 
   <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
+
+export interface AxiosInterceptorManager<T> {
+  use(resolved: ResolvedFn<T>, rejected: RejectedFn): number
+}
+
+export interface ResolvedFn<T> {
+  (val: T): T | Promise<T>
+}
+
+export interface RejectedFn {
+  (error: any): any
+}
